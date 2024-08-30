daea9487f48d4a9875d10f86bd50ced63d174c5a
thehamzakargin <hamzakargin@outlook.com>
2024-08-30 14:59:53 +0300

    phares

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 const app = express();
-
+//app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Acces-Control-Allow-Origin', '*');
+});
+res.setHeader('Acces-Control-Allow-Origin', 'GET, POST, PUT, PATCH, DELETE');
+res.setHeader('Acces-Control-Allow-Origin', 'Content-Type, Authorization');
+next();
 app.use('/feed', feedRoutes);
 app.listen(7070);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
-export.getPosts = (req, res, next) => {
-  
-}
+exports.getPosts = (req, res, next) => {
+  res.json({
+    posts: [{ title: 'Name Surname', content: 'Will generatea new' }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(200).json({
+    message: 'Post created successfully',
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
